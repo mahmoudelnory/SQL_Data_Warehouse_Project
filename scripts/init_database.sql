0ee6e00d527011df1c0fae35d53af0e0ba2a2c71
Mahmoud Elnoury <57498046+mahmoudelnory@users.noreply.github.com>
2026-05-22 23:43:20 +0300

    Add cautionary note regarding database deletion
    Emphasize the importance of backups before executing the script.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -9,6 +9,7 @@ Script Purpose:
     within the database: 'bronze', 'silver', and 'gold'.
 
 WARNING:
+	This script is for learning/development only.
     Running this script will drop the entire 'DataWarehouse' database if it exists.
     All data in the database will be permanently deleted. Proceed with caution
     and ensure you have proper backups before running this script.
